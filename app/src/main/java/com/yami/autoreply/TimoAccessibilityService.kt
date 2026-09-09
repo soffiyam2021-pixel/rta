ac3f6d6a70d0b5dc8f5e3a80724d060aa58a1db2
soffiyam2021-pixel <soffi.yam2021@gmail.com>
2026-09-09 03:06:03 -0300

    Ajustar retry de atras: 4s con ventana emergente, 3.5s sin ella

--- app/src/main/java/com/yami/autoreply/TimoAccessibilityService.kt
+++ app/src/main/java/com/yami/autoreply/TimoAccessibilityService.kt
@@ -349,7 +349,7 @@ class TimoAccessibilityService : AccessibilityService() {
                                                                   Thread.sleep(500)
                                                                                                    var backAttempts = 0
                                                                                                    // Si hubo una ventana emergente antes o despues de la respuesta, no hace falta esperar tanto entre reintentos de "atras".
-                                                                                                   val backRetryDelayMs = if (popupBefore || popupAfter) 2500L else 5000L
+                                                                                                   val backRetryDelayMs = if (popupBefore || popupAfter) 4000L else 3500L
                                                                   while (isStillInChat() && backAttempts < 3) {
                                                                                                                performGlobalAction(GLOBAL_ACTION_BACK)
                                                                                                                                                       Thread.sleep(backRetryDelayMs)
