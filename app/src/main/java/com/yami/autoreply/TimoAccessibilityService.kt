f4ea8e2b1fcfd6c8fe7f1d944bccc040af3f1550
soffiyam2021-pixel <soffi.yam2021@gmail.com>
2026-09-13 01:02:22 -0300

    Update TimoAccessibilityService.kt

--- app/src/main/java/com/yami/autoreply/TimoAccessibilityService.kt
+++ app/src/main/java/com/yami/autoreply/TimoAccessibilityService.kt
@@ -409,7 +409,7 @@ private fun clickButtonByText(root: AccessibilityNodeInfo, targetText: String, l
                                                                   Thread.sleep(500)
                                                                                                    var backAttempts = 0
                                                                                                    // Si hubo una ventana emergente antes o despues de la respuesta, no hace falta esperar tanto entre reintentos de "atras".
-                                                                                                   val backRetryDelayMs = if (popupBefore || popupAfter) 4000L else 2700L
+                                                                                                   val backRetryDelayMs = if (popupBefore || popupAfter) 2000L else 1500L
                                                                   while (isStillInChat() && backAttempts < 3) {
                                                                                                                performGlobalAction(GLOBAL_ACTION_BACK)
                                                                                                                                                       Thread.sleep(backRetryDelayMs)
